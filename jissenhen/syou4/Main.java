af772c7666d58e678ca2d5f6e1865fe8bdd8352a
kudo <your.email>
2023-05-09 07:50:17 +0900

    study function

--- jissenhen/syou4/Main.java
+++ jissenhen/syou4/Main.java
@@ -1,5 +1,16 @@
 package syou4;
+import java.util.function.Function;
 
 public class Main {
-
+	public static Integer len(String s) {
+		return s.length();
+	}
+	
+	public static void main(String[] args) {
+		//	Function<引数, 戻り値>で記述	
+		Function<String, Integer> func = Main::len;
+		
+		int a = func.apply("java");
+		System.out.println("文字列「Java」は" + a + "文字です");
+	}
 }
